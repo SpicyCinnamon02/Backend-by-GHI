39edcf104e3e846a21aab1493bf3503b3dca7330
Joshua Ramos <joshua.ramosd0302@gmail.com>
2024-10-23 21:53:31 +0800

    Fixed prices option section mobile responsiveness

--- src/pages/home/Prices.jsx
+++ src/pages/home/Prices.jsx
@@ -26,11 +26,11 @@ export const priceOpt = [
 ]
 const Prices = () => {
     return (
-        <div className='flex justify-center items-center gap-5 py-10'>
+        <div className='flex justify-center items-center flex-wrap gap-5 py-10'>
             {priceOpt.map((prices) => (
-                <div className='flex justify-between items-start flex-col shadow-lg h-[700px] w-[400px] border border-green-light rounded-xl lg:px-[50px] lg:py-[30px] transform hover:scale-105 transition-all duration-300'>
-                    <div className='border-b border-gray-400 pb-[20px]'>
-                        <p className='text-[20px] lg:text-[40px] font-black text-green-darker'>{prices.title}</p>
+                <div className='flex justify-between items-center lg:items-start flex-col shadow-lg h-[600px] w-[350px] lg:h-[700px] lg:w-[400px] border border-green-light rounded-xl py-5 lg:px-[50px] lg:py-[30px] transform lg:hover:scale-105 transition-all duration-300'>
+                    <div className='border-b border-gray-400 pb-[20px] flex flex-col justify-center items-center lg:justify-start lg:items-start'>
+                        <p className='text-[28px] lg:text-[40px] font-black text-green-darker'>{prices.title}</p>
                         <p className='text-20px '>{prices.description}</p>
                     </div>
                     <div className='flex justify-center items-start flex-col lg:gap-2'>
@@ -48,7 +48,7 @@ const Prices = () => {
                     <div className='flex justify-center items-center flex-col w-full gap-3'>
                         <p className='text-[28px] font-semibold text-green-mid'>{prices.price}</p>
                         <button
-                        className="px-3 py-2 lg:px-0 lg:py-2 text-[14px] lg:text-[18px] lg:w-[150px] bg-green-light text-white font-semibold rounded-lg shadow-md hover:bg-green-darker focus:outline-none hover:text-white focus:ring-2 focus:ring-opacity-75 transition duration-300"
+                        className="px-3 py-2 lg:px-0 lg:py-2 text-[14px] lg:text-[18px] w-[100px] lg:w-[150px] bg-green-light text-white font-semibold rounded-lg shadow-md hover:bg-green-darker focus:outline-none hover:text-white focus:ring-2 focus:ring-opacity-75 transition duration-300"
                     >
                             {prices.button}
                         </button>
